3d36330f1bf59c2a01f32bcdbfcabcae57e19a18
Pdawg11239 <pdawg11239linusfan@gmail.com>
2023-02-28 13:08:58 -0500

    add source + initial commit

---
+++ CompositionTest/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.System.Diagnostics;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace CompositionTest
+{
+    public sealed partial class MainPage : Page
+    {
+        ContainerVisual visual;
+        Compositor compositor;
+        string drawTime;
+
+        static Random rnd = new Random();
+        static int size = 6;
+        static int columnGrid = 99;
+        Vector2 sizeVector = new Vector2(size);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            ObjectInput.Text = 100.ToString();
+            GridSizeInput.Text = 6.ToString();
+            ProcessMemoryUsageReport memoryUsageLaunch = ProcessDiagnosticInfo.GetForCurrentProcess().MemoryUsage.GetReport();
+            MemoryUsage.Text = "Memory usage for draw (working set): " + (memoryUsageLaunch.WorkingSetSizeInBytes / 1024).ToString() + "kb";
+
+            compositor = Window.Current.Compositor;
+            visual = compositor.CreateContainerVisual();
+            ElementCompositionPreview.SetElementChildVisual(DrawArea, visual);
+        }
+
+        private async void Draw_Click(object sender, RoutedEventArgs e)
+        {
+            VisualStatus.Text = "Creating visuals...";
+            await Task.Run(() =>
+            {
+                visual.Children.RemoveAll();
+
+                Debug.WriteLine("Creating visuals...");
+                Stopwatch drawTimeBench = Stopwatch.StartNew();
+
+                for (int intT = 0; intT < columnGrid; intT++)
+                {
+                    for (int incR = 0; incR < columnGrid; incR++)
+                    {
+                        SpriteVisual b = compositor.CreateSpriteVisual();
+                        b.Size = sizeVector;
+                        b.Brush = compositor.CreateColorBrush(Windows.UI.Color.FromArgb(0xff, Convert.ToByte(rnd.Next(0, 256)), Convert.ToByte(rnd.Next(0, 256)), Convert.ToByte(rnd.Next(0, 256))));
+                        b.Offset = new Vector3(intT * size, incR * size, 0);
+                        visual.Children.InsertAtBottom(b);
+                    }
+                }
+
+                drawTimeBench.Stop();
+                Debug.WriteLine("Visuals created in: " + drawTimeBench.Elapsed.ToString("mm\\:ss\\.fff"));
+                drawTime = drawTimeBench.Elapsed.ToString("mm\\:ss\\.fff");
+            });
+            ProcessMemoryUsageReport memoryUsageRun = ProcessDiagnosticInfo.GetForCurrentProcess().MemoryUsage.GetReport();
+            MemoryUsage.Text = "Memory usage for draw (working set): " + (memoryUsageRun.WorkingSetSizeInBytes / 1024).ToString() + "kb";
+            VisualStatus.Text = "Visuals created in: " + drawTime;
+        }
+
+        private void ObjectInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                columnGrid = Convert.ToInt32(ObjectInput.Text);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private void GridSizeInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                size = Convert.ToInt32(GridSizeInput.Text);
+                sizeVector = new Vector2(size);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
